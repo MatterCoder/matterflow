cf584bf69fadc59954604ea81edbb29fe1d54f9d
Ivo Brett <oideibrett@lccgmail.com>
2024-10-13 09:21:41 +0100

    fixed up JMESpath tester

--- web/src/Components/JMESPathTester.jsx
+++ web/src/Components/JMESPathTester.jsx
@@ -5,10 +5,8 @@ import { DragOutlined } from "@ant-design/icons";
 import JsonDataInput from "./JsonDataInput";
 
 const JMESPathTester = () => {
-  const [expression, setExpression] = useState(
-    '[?node_id == `1`].attributes."0"."40"."1"'
-  );
-  const [jsonData, setJsonData] = useState();
+  const [expression, setExpression] = useState('');
+  const [jsonData, setJsonData] = useState('');
   const [result, setResult] = useState("");
 
   useEffect(() => {
@@ -21,7 +19,12 @@ const JMESPathTester = () => {
       const searchResult = jmespath.search(data, expression);
       setResult(JSON.stringify(searchResult, null, 2));
     } catch (error) {
-      setResult(`Error: ${error.message}`);
+      if (jsonData == '' || expression == '') {       
+        setResult(`Try testng a JMESPath expression against your Sample JSON data and see the result here`);
+      }
+      else {
+        setResult(`Error: ${error.message}`);
+      }
     }
   };
 
